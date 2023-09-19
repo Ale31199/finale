81f711eb2ecb5d72fe89875ab2909dc00ad0ced8
Alessio Santillo <alessiosantillo31199@gmail.com>
2023-09-19 18:11:09 +0200

    pagine finite, mancano collegamenti link ai progetti

--- src/header.js
+++ src/header.js
@@ -1,51 +1,9 @@
 import {Outlet, Link} from 'react-router-dom';
-import React, {useState} from 'react';
+import React from 'react';
 import './header.css';
 
 
  const Header=()=>{
-const [sel, setsel] = useState(false);
-const [sel2, setsel2] = useState(false);
-const [sel3, setsel3] = useState(false);
-
-
-const selected=()=>{
-  if (sel){
-    setsel(false)
-  } else {
-    setsel(true)
-  }
-}
-const selected2=()=>{
-  if (sel2){
-    setsel2(false)
-  } else {
-    setsel2(true)
-  }
-}
-const selected3=()=>{
-  if (sel3){
-    setsel3(false)
-  } else {
-    setsel3(true)
-  }
-}
-
-const home = sel ?{
-  boxShadow:'orange 0px 0px 0px 0px'
-  }
-  :{};
-
-const skills = sel2 ?{
-  boxShadow:'orange 0px 6px 0px 0px',
-  }
- 
-  :{};
-
-  const prj = sel3 ?{
-    boxShadow:'orange 0px 6px 0px 0px'
-    }
-    :{};
 
 
     return(
@@ -53,9 +11,9 @@ const skills = sel2 ?{
             <h1 className='title'>HomeCode</h1>
             <h1 className='title1'>|</h1>
             <div className='menu'>
-            <Link to="/" className='home' style={home} onClick={selected}>Home</Link>
-            <Link to="/skills" className='skills' style={skills} onClick={selected2}>Skills</Link>
-            <Link to="/progetti" className='progetti' style={prj} onClick={selected3}>Progetti</Link>
+            <Link to="/" className='home'>Home</Link>
+            <Link to="/skills" className='skills' >Skills</Link>
+            <Link to="/progetti" className='progetti' >Progetti</Link>
             </div>
         <Outlet/>
         
--- src/proj.js
+++ src/proj.js
@@ -1,16 +1,51 @@
 import calc from './img/calculator.png';
 import game from './img/gamepad.png';
-import port from'./img/portfolio.png';
 import spotify from'./img/spotify.png';
-import app from './img/app.png';
+import './proj.css';
 
 
 
 
 const proj =()=>{
     return(
-        <div>
-            <p>This is proj</p>
+        <div className='progettix'>
+
+            <div className='prospo'>
+             <img src={spotify} className='spo'></img>
+             <div className='boxspo'>
+                <h1 className='spot'>SPOTIFY</h1>
+             </div>
+             <h1 className='tspo'>Questo è il progetto Spotify! Si tratta semplicemente della pagina principale, mostrando le playlist preferite, gli artisti e altro ancora. Potrebbe avere qualche problema con la adattabilità di alcuni schermi.
+             ma per il resto dovrebbe essere tutto funzionante! Ovviamente non riproduce musica, ma è solo per il lato estetico con qualche piccola interattività. NOTA: Al momento non è visualizzabile su mobile o schermi piccoli, consiglio di visualizzarlo su PC.
+             </h1>
+             <button className='buttonspo'>Prova Ora</button>
+            </div>
+            
+
+            <div className='progame'>
+             <img src={game} className='game'></img>
+             <div className='boxgame'>
+                <h1 className='gamer'>PIXEL NIGHTMARE</h1>
+             </div>
+             <h1 className='tgame'>Pixel Nightmare è un breve gioco di prova, dove si basa sul aiutare questo personaggio di nome "Sticky" a scappare dal misterioso posto in cui si trova. Per farlo, avrai due opzioni,
+             in base a quella scelta, la trama prenderà strade diverse, bisognerà pensare bene se fare una determinata cosa oppure l'altra. Al momento è molto breve, ma sarebbe bello continuarlo, intanto qui, c'è una breve prova che puoi fare! 
+             </h1>
+             <button className='buttongame'>Prova Ora</button>
+            </div>
+
+            <div className='proca'>
+             <img src={calc} className='ca'></img>
+             <div className='boxca'>
+                <h1 className='cal'>CALCULATOR</h1>
+             </div>
+             <h1 className='tca'>Questo è il progetto Spotify! Si tratta semplicemente della pagina principale, mostrando le playlist preferite, gli artisti e altro ancora. Potrebbe avere qualche problema con la adattabilità di alcuni schermi.
+             ma per il resto dovrebbe essere tutto funzionante! Ovviamente non riproduce musica, ma è solo per il lato estetico con qualche piccola interattività.
+             </h1>
+             <button className='buttonca'>Prova Ora</button>
+            </div>
+
+
+
         </div>
     )
 }
