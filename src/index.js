5e221f0b31edf3541492e3bb552b035bf5c886b6
Alessio Santillo <alessiosantillo31199@gmail.com>
2023-10-06 12:35:34 +0200

    aggiunto firebase hosting per sito online

--- src/index.js
+++ src/index.js
@@ -25,6 +25,35 @@ import Preordinaluna from './xflies/preordinaluna';
 import Preordinaextra from './xflies/preordinaextra';
 import Xshop from './xflies/xshop';
 
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCdM7z1K_IWiIl52yYrDMo-4VXnZ-UZ4hM",
+  authDomain: "homecode-34492.firebaseapp.com",
+  projectId: "homecode-34492",
+  storageBucket: "homecode-34492.appspot.com",
+  messagingSenderId: "143673958493",
+  appId: "1:143673958493:web:598eab6d33af438c52edb4",
+  measurementId: "G-0H90NCJEJ5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
+
+
+
 export default function TheRouter(){
   return(
     <BrowserRouter>
@@ -71,3 +100,7 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
