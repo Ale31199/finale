d8cadd61c56f6618acf7913318ed22f18bf90ff2
Alessio Santillo <alessiosantillo31199@gmail.com>
2023-10-06 15:55:56 +0200

    modifica al header menu

--- src/header.js
+++ src/header.js
@@ -5,15 +5,64 @@ import './header.css';
 
  const Header=()=>{
 
+  const [homeee, sethomeee]=useState(false)
+  const [skillsss, setskillsss]=useState(false)
+  const [progettiii, setprogettiii]=useState(false)
+
+
+  const onHome=()=>{
+    if(homeee){
+      sethomeee(false)
+    } else {
+      sethomeee(true)
+      setskillsss(false)
+      setprogettiii(false)
+    }
+  }
+  const onSkill=()=>{
+    if(skillsss){
+    setskillsss(false)
+    } else {
+      setskillsss(true)
+      sethomeee(false)
+      setprogettiii(false)
+    }
+  }
+  const onProgetti=()=>{
+    if(progettiii){
+      setprogettiii(false)
+    } else {
+      setprogettiii(true)
+      sethomeee(false)
+      setskillsss(false)
+    }
+  }
+
+
+
+  const homeE = homeee ?{
+    color: 'orange',
+    boxShadow: 'orange 0px 6px 0px 0px'
+  }:{};
+  const skillsS = skillsss ?{
+    color: 'orange',
+    boxShadow: 'orange 0px 6px 0px 0px'
+  }:{};
+  const progettiI = progettiii ?{
+    color: 'orange',
+    boxShadow: 'orange 0px 6px 0px 0px'
+  }:{};
+  
+
 
     return(
         <div className='header'>
             <h1 className='titlew'>HomeCode</h1>
             <h1 className='title1x'>|</h1>
             <div className='menu'>
-            <Link to="/" className='homeq'>Home</Link>
-            <Link to="/skills" className='skills' >Skills</Link>
-            <Link to="/progetti" className='progetti' >Progetti</Link>
+            <Link to="/" className='homeq'style={homeE} onClick={onHome}>Home</Link>
+            <Link to="/skills" className='skills' style={skillsS} onClick={onSkill}>Skills</Link>
+            <Link to="/progetti" className='progetti'style={progettiI}onClick={onProgetti} >Progetti</Link>
             </div>
         <Outlet/>
         
